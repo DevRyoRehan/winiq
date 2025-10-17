9a43b71517abdf7ebe32118e8fc439b48036ba84
DevRyoRehan <ryodgtal@gmail.com>
2025-10-17 20:58:30 +0200

    Fix defineConfig import in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [
     react({
@@ -7,6 +10,6 @@ export default defineConfig({
     }),
   ],
   build: {
-    outDir: 'dist', // ✅ Explicitly define output folder
+    outDir: 'dist',
   },
 });

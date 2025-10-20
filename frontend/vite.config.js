21ce9509e5f30a5c033835d053d12138cd1d342d
DevRyoRehan <ryodgtal@gmail.com>
2025-10-20 02:45:49 +0200

    Fix vite.config.js by removing invalid Babel plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/',
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
   },
@@ -24,3 +18,4 @@ export default defineConfig({
     cors: true,
   }
 });
+
